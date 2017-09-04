0aca8ea2b9e1c98defa028144d364194a91e9e6e
Jared Ledvina <jared@techsmix.net>
2017-09-04 15:20:56 -0400

    Chapter 5 - main.go using map

--- chapter5/main.go
+++ chapter5/main.go
@@ -3,21 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    slice1 := []int{1,2,3}
-    slice2 := make([]int, 2)
-    copy(slice2, slice1)
-    fmt.Println(slice1, slice2)
-//    x := make([]float64 {
-//        98,
-//        93,
-//        77,
-//        82,
-//        83,
-//    }
-//
-//    var total float64 = 0
-//    for _, value := range x {
-//        total += value
-//    }
-//    fmt.Println(total / float64(len(x)))
+    x := make(map[string]int)
+    x["key"] = 10
+    fmt.Println(x["key"])
 }
